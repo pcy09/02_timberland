e670c20731998bb6dd9eb8cf1ba26e85777ca7f8
pcy09 <pcy09@naver.com>
2022-12-07 00:05:49 +0900

    221207

--- js/main.js
+++ js/main.js
@@ -1,61 +1,65 @@
+/* a href='#' 클릭 무시 */
+$('a[href="#"]').click(function (event) {
+	event.preventDefault();
+});
+
 // Initialize Swiper
-
-if($(window).width()>1024){
-    var swiper = new Swiper(".mySwiper", {
-        navigation: {
-            nextEl: ".swiper-button-next",
-            prevEl: ".swiper-button-prev"
-        },
-        slidesPerView: 4,
-        spaceBetween: 30,
-        pagination: {
-            el: ".swiper-pagination",
-            clickable: true
-        }
-    });
+if ($(window).width() > 1024) {
+	var swiper = new Swiper(".mySwiper", {
+		navigation: {
+			nextEl: ".swiper-button-next",
+			prevEl: ".swiper-button-prev",
+		},
+		slidesPerView: 4,
+		spaceBetween: 30,
+		pagination: {
+			el: ".swiper-pagination",
+			clickable: true,
+		},
+	});
 }
-if($(window).width()<=1024){
-    var swiper = new Swiper(".mySwiper", {
-        navigation: {
-            nextEl: ".swiper-button-next",
-            prevEl: ".swiper-button-prev"
-        },
-        slidesPerView: 2.5,
-        spaceBetween: 10,
-        pagination: {
-            el: ".swiper-pagination",
-            clickable: true
-        }
-    });
+if ($(window).width() <= 1024) {
+	var swiper = new Swiper(".mySwiper", {
+		navigation: {
+			nextEl: ".swiper-button-next",
+			prevEl: ".swiper-button-prev",
+		},
+		slidesPerView: 2.5,
+		spaceBetween: 10,
+		pagination: {
+			el: ".swiper-pagination",
+			clickable: true,
+		},
+	});
 }
 
-$(window).resize(function(){
-    if($(window).width()>1024){
-        var swiper = new Swiper(".mySwiper", {
-            navigation: {
-                nextEl: ".swiper-button-next",
-                prevEl: ".swiper-button-prev"
-            },
-            slidesPerView: 4,
-            spaceBetween: 30,
-            pagination: {
-                el: ".swiper-pagination",
-                clickable: true
-            }
-        });
-    }
-    if($(window).width()<=1024){
-        var swiper = new Swiper(".mySwiper", {
-            navigation: {
-                nextEl: ".swiper-button-next",
-                prevEl: ".swiper-button-prev"
-            },
-            slidesPerView: 2.5,
-            spaceBetween: 10,
-            pagination: {
-                el: ".swiper-pagination",
-                clickable: true
-            }
-        });
-    }
+$(window).resize(function () {
+	if ($(window).width() > 1024) {
+		var swiper = new Swiper(".mySwiper", {
+			navigation: {
+				nextEl: ".swiper-button-next",
+				prevEl: ".swiper-button-prev",
+			},
+			slidesPerView: 4,
+			spaceBetween: 30,
+			pagination: {
+				el: ".swiper-pagination",
+				clickable: true,
+			},
+		});
+	}
+	if ($(window).width() <= 1024) {
+		var swiper = new Swiper(".mySwiper", {
+			navigation: {
+				nextEl: ".swiper-button-next",
+				prevEl: ".swiper-button-prev",
+			},
+			slidesPerView: 2.5,
+			spaceBetween: 10,
+			pagination: {
+				el: ".swiper-pagination",
+				clickable: true,
+			},
+		});
+	}
 });
