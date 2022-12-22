188de5accb3dba21ac38628496b9352ead2ec131
pcy09 <pcy09@naver.com>
2022-12-22 22:49:36 +0900

    swiper 수정

--- js/main.js
+++ js/main.js
@@ -4,62 +4,21 @@ $('a[href="#"]').click(function (event) {
 });
 
 // Initialize Swiper
-if ($(window).width() > 1024) {
-	var swiper = new Swiper(".mySwiper", {
-		navigation: {
-			nextEl: ".swiper-button-next",
-			prevEl: ".swiper-button-prev",
-		},
-		slidesPerView: 4,
-		spaceBetween: 30,
-		pagination: {
-			el: ".swiper-pagination",
-			clickable: true,
-		},
-	});
-}
-if ($(window).width() <= 1024) {
-	var swiper = new Swiper(".mySwiper", {
-		navigation: {
-			nextEl: ".swiper-button-next",
-			prevEl: ".swiper-button-prev",
-		},
-		slidesPerView: 2.5,
-		spaceBetween: 10,
-		pagination: {
-			el: ".swiper-pagination",
-			clickable: true,
-		},
-	});
-}
-
-$(window).resize(function () {
-	if ($(window).width() > 1024) {
-		var swiper = new Swiper(".mySwiper", {
-			navigation: {
-				nextEl: ".swiper-button-next",
-				prevEl: ".swiper-button-prev",
-			},
+var swiper = new Swiper(".mySwiper", {
+	navigation: {
+		nextEl: ".swiper-button-next",
+		prevEl: ".swiper-button-prev",
+	},
+	slidesPerView: 2.5,
+	spaceBetween: 10,
+	pagination: {
+		el: ".swiper-pagination",
+		clickable: true,
+	},
+	breakpoints: {
+		1024: {
 			slidesPerView: 4,
 			spaceBetween: 30,
-			pagination: {
-				el: ".swiper-pagination",
-				clickable: true,
-			},
-		});
-	}
-	if ($(window).width() <= 1024) {
-		var swiper = new Swiper(".mySwiper", {
-			navigation: {
-				nextEl: ".swiper-button-next",
-				prevEl: ".swiper-button-prev",
-			},
-			slidesPerView: 2.5,
-			spaceBetween: 10,
-			pagination: {
-				el: ".swiper-pagination",
-				clickable: true,
-			},
-		});
-	}
+		},
+	},
 });
